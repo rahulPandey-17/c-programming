214fdc1cd5f0366d6f34a3f5554ae4f304988c12
rahul_pandey <0517rp.rahul@gmail.com>
2024-07-18 18:55:19 +0530

    started c language course

---
+++ c_language_course/Chapter_1/01_variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int number;   // variable declaration
+    number = 10; // variable initialisation
+
+    // both variable declaration and initialisation can be done in one statement
+
+    int number_1 = 20;
+
+    // printing values of both the variables
+
+    printf("%d\n", number);
+    printf("%d\n", number_1);
+    
+    return 0;
+
+}
